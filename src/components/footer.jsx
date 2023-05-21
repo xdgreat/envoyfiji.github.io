bce826ec200de9a97acf74a183a20e647cdc50ef
nik <nnaiker01@gmail.com>
2023-05-22 02:38:03 +1200

    github icon added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -14,19 +14,37 @@ const Footer = () => {
               </h2>
             </div>
             <div className="footer-column-two">
-              <a href="#motivation" className="footer-text">Motivation</a>
-              <a href="#fitness" className="footer-text">Fitness Hub</a>
-              <a href="#history" className="footer-text">Our History</a>
+              <a href="#motivation" className="footer-text">
+                Motivation
+              </a>
+              <a href="#fitness" className="footer-text">
+                Fitness Hub
+              </a>
+              <a href="#history" className="footer-text">
+                Our History
+              </a>
             </div>
             <div className="footer-column-three">
-              <a href="#newsletter" className="footer-text">Newsletter</a>
-              <a href="#testimonials" className="footer-text">Testimonials</a>
+              <a href="#newsletter" className="footer-text">
+                Newsletter
+              </a>
+              <a href="#testimonials" className="footer-text">
+                Testimonials
+              </a>
             </div>
             <div className="footer-column-four">
-              <a href="/" className="footer-text">Home</a>
-              <a href="/about" className="footer-text">Contact Us</a>
-              <a href="" className="footer-text">About</a>
-              <a href="/product" className="footer-text">Products</a>
+              <a href="/" className="footer-text">
+                Home
+              </a>
+              <a href="/about" className="footer-text">
+                Contact Us
+              </a>
+              <a href="" className="footer-text">
+                About
+              </a>
+              <a href="/product" className="footer-text">
+                Products
+              </a>
             </div>
           </div>
           <hr />
@@ -36,9 +54,14 @@ const Footer = () => {
             <i class="fa-brands fa-facebook"></i>
             <i class="fa-brands fa-facebook-messenger"></i>
             <i class="fa-solid fa-wifi"></i>
+            <a href=".https://github.com/xdgreat/envoyfijireactproject.git">
+              <i class="fa-brands fa-github"></i>
+            </a>
           </div>
           <div className="footer-row-three">
-            <p className="copyright footer-text">©Copyright. All rights reserved.</p>
+            <p className="copyright footer-text">
+              ©Copyright. All rights reserved.
+            </p>
           </div>
         </div>
       </footer>
